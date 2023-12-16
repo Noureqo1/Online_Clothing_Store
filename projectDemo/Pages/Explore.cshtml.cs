e63855c3687396c82f2ad812cae38e47e7718cbb
noureqo <128331928+noureqo@users.noreply.github.com>
2023-12-16 22:40:42 +0200

    all pages added

---
+++ projectDemo/Pages/Explore.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectDemo.Pages
+{
+    public class ExploreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ projectDemo/Pages/Order.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectDemo.Pages
+{
+    public class OrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
