1e108a211ea9bbb64e3054294b5a0a0c91199ae3
noureqo <128331928+noureqo@users.noreply.github.com>
2023-12-17 00:25:39 +0200

    Explore added

--- projectDemo/Pages/Index.cshtml.cs
+++ projectDemo/Pages/Index.cshtml.cs
@@ -8,49 +8,8 @@ namespace ProjectDemo.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-        public models.users tempUser { get; set; }
-        public List<models.users> usersList { get; set; }
-        public void OnGET()
-        {
-            string connectionString = "Data Source=LAPTOP-OH72TN5U;Initial Catalog=projectdemo;Integrated Security=True";
-
-            SqlConnection connection = new SqlConnection(connectionString);
-
-            string query1 = "SELECT * FROM users";
-
-            try
-            {
-               connection.Open();
-               SqlCommand cmd = new SqlCommand(query1, connection);
-               SqlDataReader reader = cmd.ExecuteReader();
-
-                usersList = new List<models.users>();
-                while (reader.Read())
-                {
-                    tempUser = new models.users();
-
-                    tempUser.id =int.Parse(reader[0].ToString());
-                    tempUser.name = reader[1].ToString(); 
-                    tempUser.email = reader[2].ToString();
-
-                    usersList.Add(tempUser);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            finally
-            {
-                connection.Close();
-            }
-
-        }
+        
     }
 }
